7314eccace2e4e246f5e438e547857933997cfeb
Kachhadiya Dharmik <99231778+dharmik2003@users.noreply.github.com>
2024-05-15 14:06:20 +0530

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
+    basePath:"/dharmik2003/PortFolio-Dharmik",
+    // output: "export",
+    
 };
 
 export default nextConfig;
